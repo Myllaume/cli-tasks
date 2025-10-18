2fd995c06ba9ac8966f93364f102edc8592c4967
Myllaume <guillaume.brioudes@myllaume.fr>
2025-10-18 15:27:43 +0200

    fix: add app name to data dirs

--- src/main/java/task/cli/myllaume/AppDirs.java
+++ src/main/java/task/cli/myllaume/AppDirs.java
@@ -7,23 +7,24 @@ public class AppDirs {
   private final String configDir;
   private final String cacheDir;
   private final String dataDir;
+  private final String appName = "myllaume-tasks";
 
   public AppDirs() {
     String homeDir = System.getProperty("user.home");
     String osName = System.getProperty("os.name").toLowerCase();
 
     if (osName.contains("win")) {
-      this.configDir = System.getenv("APPDATA");
-      this.cacheDir = System.getenv("LOCALAPPDATA");
-      this.dataDir = System.getenv("LOCALAPPDATA");
+      this.configDir = System.getenv("APPDATA") + "/" + appName;
+      this.cacheDir = System.getenv("LOCALAPPDATA") + "/" + appName;
+      this.dataDir = System.getenv("LOCALAPPDATA") + "/" + appName;
     } else if (osName.contains("mac")) {
-      this.configDir = homeDir + "/Library/Application Support";
-      this.cacheDir = homeDir + "/Library/Caches";
-      this.dataDir = homeDir + "/Library/Application Support";
+      this.configDir = homeDir + "/Library/Application Support" + "/" + appName;
+      this.cacheDir = homeDir + "/Library/Caches" + "/" + appName;
+      this.dataDir = homeDir + "/Library/Application Support" + "/" + appName;
     } else {
-      this.configDir = homeDir + "/.config";
-      this.cacheDir = homeDir + "/.cache";
-      this.dataDir = homeDir + "/.local/share";
+      this.configDir = homeDir + "/.config" + "/" + appName;
+      this.cacheDir = homeDir + "/.cache" + "/" + appName;
+      this.dataDir = homeDir + "/.local/share" + "/" + appName;
     }
 
     if (this.configDir == null || this.cacheDir == null || this.dataDir == null) {

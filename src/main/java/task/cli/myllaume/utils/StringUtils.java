89d722a18dda8205701489d32967124b79849758
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2025-10-18 21:50:49 +0200

    feat(gui): static files

--- src/main/java/task/cli/myllaume/utils/StringUtils.java
+++ src/main/java/task/cli/myllaume/utils/StringUtils.java
@@ -12,4 +12,39 @@ public class StringUtils {
     normalized = normalized.toLowerCase();
     return normalized;
   }
+
+  public static String escapeHtml(String input) {
+    if (input == null) {
+      return null;
+    }
+    StringBuilder escaped = new StringBuilder();
+    for (char c : input.toCharArray()) {
+      switch (c) {
+        case '<':
+          escaped.append("&lt;");
+          break;
+        case '>':
+          escaped.append("&gt;");
+          break;
+        case '&':
+          escaped.append("&amp;");
+          break;
+        case '"':
+          escaped.append("&quot;");
+          break;
+        case '\'':
+          escaped.append("&#x27;");
+          break;
+        case '/':
+          escaped.append("&#x2F;");
+          break;
+        case '`':
+          escaped.append("&#x60;");
+          break;
+        default:
+          escaped.append(c);
+      }
+    }
+    return escaped.toString();
+  }
 }
